b6af0e8e624a3308e464e2a71ae516aca247087f
srava <srava@DESKTOP-32HGK8C.home>
2022-03-30 04:34:03 +0100

    Tags

---
+++ CucumberProjectReal/src/test/java/com/stepDefinition/TagsDemo.java
@@ -0,0 +1,47 @@
+package com.stepDefinition;
+
+import io.cucumber.java.en.Given;
+
+public class TagsDemo {
+	@Given("This is Scenario1")
+	public void this_is_scenario1() {
+	   
+	}
+
+	@Given("This is Scenario2")
+	public void this_is_scenario2() {
+	  
+	}
+
+	@Given("This is Scenario3")
+	public void this_is_scenario3() {
+	   
+	}
+
+	@Given("This is Scenario4")
+	public void this_is_scenario4() {
+	   
+	}
+
+	@Given("This is Scenario5")
+	public void this_is_scenario5() {
+	   
+	}
+
+	@Given("This is Scenario6")
+	public void this_is_scenario6() {
+	   
+	}
+
+	@Given("This is Scenario7")
+	public void this_is_scenario7() {
+	}
+
+	@Given("This is Scenario8")
+	public void this_is_scenario8() {
+	   
+	}
+
+
+
+}
---
+++ CucumberProjectReal/src/test/resources/FeatureFiles/Tags.feature
@@ -0,0 +1,26 @@
+Feature: using of Tags in Cucumber
+@SmokeTest
+Scenario: Scenario1
+Given This is Scenario1
+@SanityTest
+Scenario: Scenario2
+Given This is Scenario2
+
+Scenario: Scenario3
+Given This is Scenario3
+
+Scenario: Scenario4
+Given This is Scenario4
+
+Scenario: Scenario5
+Given This is Scenario5
+
+Scenario: Scenario6
+Given This is Scenario6
+@SmokeTest
+@SanityTest
+Scenario: Scenario7
+Given This is Scenario7
+
+Scenario: Scenario8
+Given This is Scenario8
